7e72a8b2ffc8ab837d6ae81812e1c01fdca12f2a
AaqibRaina <aaqibraina786@gmail.com>
2024-12-11 20:57:51 +0530

    Gym management using php along with project report uploaded

---
+++ gym/about.php
@@ -0,0 +1,105 @@
+<?php 
+session_start();
+error_reporting(0);
+include 'include/config.php';
+$uid=$_SESSION['uid'];
+
+if(isset($_POST['submit']))
+{ 
+$pid=$_POST['pid'];
+
+
+$sql="INSERT INTO tblbooking (package_id,userid) Values(:pid,:uid)";
+
+$query = $dbh -> prepare($sql);
+$query->bindParam(':pid',$pid,PDO::PARAM_STR);
+$query->bindParam(':uid',$uid,PDO::PARAM_STR);
+$query -> execute();
+echo "<script>alert('Package has been booked.');</script>";
+echo "<script>window.location.href='booking-history.php'</script>";
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>Gym Management System</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Ahana Yoga HTML Template">
+	<meta name="keywords" content="yoga, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/nice-select.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	
+
+	<!-- Header Section -->
+	<?php include 'include/header.php';?>
+	<!-- Header Section end -->
+
+	
+
+	                                                                              
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="img/bg2.jpg" style="height:600px;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2 style="color:#fffff;text-shadow:3px 3px 3px #323232;margin-top:25%;">Who &nbspWe &nbspAre</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<!-- Pricing Section -->
+	<section class="pricing-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<img src="img/icons/logo-icon.png" alt="">
+				<h2>About Us</h2>
+			</div>
+			<div class="row">
+
+				<div class="col-lg-12 col-sm-6">
+			<p>Power House Gym is more than just a place to work out - it's a community of fitness enthusiasts, dedicated trainers, and like-minded individuals all working towards the same goal: to become the best version of themselves. We understand the importance of not only physical fitness, but also mental and emotional well-being. That's why our state-of-the-art facility offers a wide range of equipment and classes, as well as expert trainers and nutritionists to help you reach your fitness goals. We believe that fitness is a lifestyle, and we are here to support you every step of the way. Come join us and see what sets Power House Gym apart from the rest!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+
+	<!-- Footer Section -->
+	<?php include 'include/footer.php'; ?>
+	<!-- Footer Section end -->
+
+	<div class="back-to-top"><img src="img/icons/up-arrow.png" alt=""></div>
+
+	<!-- Search model end -->
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ gym/include/footer.php
@@ -0,0 +1,19 @@
+<footer class="">
+		<div class="container">
+			
+			<div class="footer-bottom">
+				<div class="row">
+					
+					<div class="col-md-8 text-md-right">
+						<div class="copyright" style="color:#333;"> 
+						Unleash your inner powerhouse at Power House Gym!</div>
+				<p>©️ <?php echo date('Y');?> Gym Management System</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--<script>
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
+</script>-->
+	</footer>
